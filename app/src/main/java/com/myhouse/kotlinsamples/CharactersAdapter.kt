b8afc61d754e03584dbd97bc993322b006003e8d
Joseph E. Martinez <joseph.martinez@mapquest.com>
2020-10-16 20:14:28 -0600

    adding code to load the character image via glide

--- app/src/main/java/com/myhouse/kotlinsamples/CharactersAdapter.kt
+++ app/src/main/java/com/myhouse/kotlinsamples/CharactersAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.myhouse.kotlinsamples.network.CharacterResult
 
 class CharactersAdapter(private val characters: List<CharacterResult>, private val context: Context) : RecyclerView.Adapter<CharactersAdapter.CharacterViewHolder>() {
@@ -14,6 +16,7 @@ class CharactersAdapter(private val characters: List<CharacterResult>, private v
     }
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        Glide.with(context).load(characters[position].image).into(holder.imageView)
         holder.nameTextView.text = characters[position].name
         holder.speciesTextView.text = characters[position].species
         holder.statusTextView.text = characters[position].status
@@ -24,9 +27,9 @@ class CharactersAdapter(private val characters: List<CharacterResult>, private v
     }
 
     inner class CharacterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.image)
         val nameTextView: TextView = view.findViewById(R.id.name)
         val speciesTextView: TextView = view.findViewById(R.id.species)
         val statusTextView: TextView = view.findViewById(R.id.status)
-
     }
 }
